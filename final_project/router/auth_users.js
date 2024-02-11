6e3289f1248577b58aa518b757de209a2d2e4e70
Schmaddin <Martin.wuerflein@gmx.de>
2024-02-11 15:35:27 +0100

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -25,8 +25,25 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = extractUser(extractToken(req));
+    if(!books[req.params.isbn]) {
+        return res.status(404).json({message: "Not found"});
+    }
+    books[req.params.isbn].reviews[username] = req.body;
+    return res.status(201).json(books[req.params.isbn]);
+});
+
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    if(!books[req.params.isbn]) {
+        return res.status(404).json({message: "Not found"});
+    }
+    const username = extractUser(extractToken(req));
+    if(!books[req.params.isbn].reviews[username]) {
+        return res.status(200).json(books[req.params.isbn]);
+    }
+    delete books[req.params.isbn].reviews[username]
+    return res.status(200).json(books[req.params.isbn]);
 });
 
 
