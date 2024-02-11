8198f0ab025044661fad83d4ae8a92ee8c930636
Schmaddin <Martin.wuerflein@gmx.de>
2024-02-11 15:35:14 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,34 +19,56 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(200).json(books);
+    let returnPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+        resolve(books)
+        },1000)});
+    
+    returnPromise.then((result) => { return res.status(200).json(result)});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    return books[req.params.isbn] ? res.status(200).json(books[req.params.isbn]) : res.status(404).json({message: "Not found"});
+    let returnPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+        resolve(books)
+        },1000)});
+    returnPromise.then((result) => { return result[req.params.isbn] ? res.status(200).json(result[req.params.isbn]) : res.status(404).json({message: "Not found"})});
+    return ;
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    let authorBooks = {}
-    for(isbn in books) {
-        if (books[isbn].author===req.params.author) {
-            authorBooks[isbn] = books[isbn];
+    let returnPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+        resolve(books)
+        },1000)});
+    returnPromise.then((result) => {
+        let authorBooks = {}
+        for(isbn in result) {
+            if (result[isbn].author===req.params.author) {
+                authorBooks[isbn] = result[isbn];
+            }
         }
-    }
-    return res.status(200).json(authorBooks);
+        return res.status(200).json(authorBooks);
+    });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    let titleBooks = {}
-    for(isbn in books) {
-        if (books[isbn].title===req.params.title) {
-            titleBooks[isbn] = books[isbn];
+    let returnPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+        resolve(books)
+        },1000)});
+    returnPromise.then((result) => {
+        let titleBooks = {}
+        for(isbn in books) {
+            if (books[isbn].title===req.params.title) {
+                titleBooks[isbn] = books[isbn];
+            }
         }
-    }
-    return res.status(200).json(titleBooks);
+        return res.status(200).json(titleBooks);
+    });
 });
 
 //  Get book review
