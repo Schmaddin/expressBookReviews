fcd6d82503d17843ca626300590345b83f048f36
Schmaddin <Martin.wuerflein@gmx.de>
2024-02-11 11:57:38 +0100

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -3,15 +3,21 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-
+const extractToken = require('./router/auth_users.js').extractToken;
 const app = express();
+const JWT_SECRET = require('./router/auth_users.js').JWT_SECRET;
 
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const token = extractToken(req);
+    if(!token ? false : jwt.verify(token, JWT_SECRET))
+    {
+        req.next();
+    }
+    res.end('failed');
 });
  
 const PORT =5000;
